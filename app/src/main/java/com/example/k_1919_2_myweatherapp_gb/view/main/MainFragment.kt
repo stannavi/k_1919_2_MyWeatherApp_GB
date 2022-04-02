d7b7fa8a0f78d62f8c273360a8b0787bf88c297c
Stan_Navi <stanlevit@yandex.ru>
2022-04-02 15:06:23 +0300

    пытались предотвратить утечку памяти, но не получилось

--- app/src/main/java/com/example/k_1919_2_myweatherapp_gb/view/main/MainFragment.kt
+++ app/src/main/java/com/example/k_1919_2_myweatherapp_gb/view/main/MainFragment.kt
@@ -16,7 +16,12 @@ import com.example.k_1919_2_myweatherapp_gb.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
 
-    lateinit var binding:MainFragmentBinding// утечка памяти
+    lateinit var binding: MainFragmentBinding// утечка памяти
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //binding = null
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,12 +35,12 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnOne.setOnClickListener{ }
+        binding.btnOne.setOnClickListener { }
         // view.findViewById<TextView>(R.id.btnOne).setOnClickListener { }
         // view.findViewById<Button>(R.id.btnOne).setOnClickListener { }
 
         val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        val observer = object:Observer<Any>{
+        val observer = object : Observer<Any> {
             override fun onChanged(data: Any) {
                 renderData(data)
             }
@@ -45,7 +50,7 @@ class MainFragment : Fragment() {
         viewModel.getWeather()
     }
 
-    private fun renderData(data:Any) {
+    private fun renderData(data: Any) {
         Toast.makeText(requireContext(), "РАБОТАЕТ", Toast.LENGTH_SHORT).show()
     }
 
