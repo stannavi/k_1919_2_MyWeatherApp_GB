4d30f450b5e045fea80803efa2383f467302f607
Stan_Navi <stanlevit@yandex.ru>
2022-05-09 15:56:39 +0300

    урок 6 начали, добавили потокобезопасную "обработку" в фоне

--- app/src/main/java/com/example/k_1919_2_myweatherapp_gb/view/MainActivity.kt
+++ app/src/main/java/com/example/k_1919_2_myweatherapp_gb/view/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.k_1919_2_myweatherapp_gb.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import com.example.k_1919_2_myweatherapp_gb.R
+import com.example.k_1919_2_myweatherapp_gb.view.details.ThreadsLesson6.ThreadsFragment
 import com.example.k_1919_2_myweatherapp_gb.view.weatherlist.WeatherListFragment
 
 class MainActivity : AppCompatActivity() {
@@ -15,4 +19,21 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, WeatherListFragment.newInstance()).commit()
         }
     }
+
+    //lesson 6
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_threads->{
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, ThreadsFragment.newInstance()).commit()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
