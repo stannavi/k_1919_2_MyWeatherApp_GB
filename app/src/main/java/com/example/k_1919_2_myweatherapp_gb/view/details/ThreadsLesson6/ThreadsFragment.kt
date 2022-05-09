1bc979537b5e13ccda082d9b9bc3aaba2b8bd550
Stan_Navi <stanlevit@yandex.ru>
2022-05-09 17:40:25 +0300

    урок 6 добавили "вечный поток"

--- app/src/main/java/com/example/k_1919_2_myweatherapp_gb/view/details/ThreadsLesson6/ThreadsFragment.kt
+++ app/src/main/java/com/example/k_1919_2_myweatherapp_gb/view/details/ThreadsLesson6/ThreadsFragment.kt
@@ -6,6 +6,7 @@ import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -48,21 +49,45 @@ class ThreadsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val myThreads = MyThreads()
+        myThreads.start()
         with(binding) {
-            button.setOnClickListener {
+            val time = editText.text.toString().toLong()
+            var counter = 0
+            button1.setOnClickListener {
                 Thread {
-                    val time = editText.text.toString().toLong()
                     sleep(time * 1000L)
                     requireActivity().runOnUiThread {
-                        textView.text = "Плотно поработали ${time} сек"
+                        textView1.text = "${time} сек. 1"
                     }
-                }.start()
+                    Handler(Looper.getMainLooper()).post {
+                        textView1.text = "${time} сек. 2"
+                        createTextView("${Thread.currentThread().name} ${++counter}")
+                    }
+            }.start()
+        }
+
+        //"вечный поток"
+        button2.setOnClickListener {
+            myThreads.mHandler?.post {
+                sleep(time * 1000L)
+                Handler(Looper.getMainLooper()).post {
+                    textView2.text = "${time} сек. 2"
+                    createTextView("${Thread.currentThread().name} ${++counter}")
+                }
             }
         }
     }
+}
+
+    private fun createTextView(name: String) {
+       binding.mainContainer.addView(TextView(requireContext()).apply {
+            text = name
+            textSize = 14f
+    })
+}
 
     class MyThreads: Thread() {
-        lateinit var mHandler: Handler
+        var mHandler: Handler? = null
         override fun run() {
             Looper.prepare()
             mHandler = Handler(Looper.myLooper()!!)
